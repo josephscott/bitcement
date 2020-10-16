7c7cb81c96bb06d33447d18a9f4d263b048b92b1
Joseph Scott <joseph@josephscott.org>
2020-10-16 15:11:00 -0600

    Add anti-csrf twig function

--- config/twig.php
+++ config/twig.php
@@ -1,4 +1,6 @@
 <?php
+use \ParagonIE\AntiCSRF\AntiCSRF;
+
 $app->inject(
 	'twig',
 	new \Twig\Environment(
@@ -7,5 +9,19 @@ $app->inject(
 				'cache' => APP_DIR . '/cache/twig',
 				'auto_reload' => true
 			]
+	)
+);
+
+$app->use( 'twig' )->addFunction(
+	new \Twig\TwigFunction(
+		'form_token',
+		function( $lock_to = null ) {
+			static $csrf;
+			if ( $csrf === null ) {
+				$csrf = new AntiCSRF();
+			}
+			return $csrf->insertToken($lock_to, false);
+		},
+		['is_safe' => ['html']]
 	)
 );
