0ec29f6a7768d43ef5754158611f955cfe22d6c0
Joseph Scott <joseph@josephscott.org>
2020-10-15 23:52:08 -0600

    Move home.php to index.php

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,5 @@
 <?php
-$app->route( 'GET', '/', APP_DIR . '/routes/home.php' );
+$app->route( 'GET', '/', APP_DIR . '/routes/index.php' );
 
 $app->route( 'GET', '/about', APP_DIR . '/routes/about.php' );
 $app->route( 'POST', '/about', APP_DIR . '/routes/about.php' );
