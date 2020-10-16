3b09402da19be9cafe32781010933b73aced6451
Joseph Scott <joseph@josephscott.org>
2020-10-15 23:38:35 -0600

    Load config files

--- init.php
+++ init.php
@@ -5,3 +5,7 @@ define( 'APP_DIR', __DIR__ );
 require APP_DIR . '/vendor/autoload.php';
 
 $app = new BitCement\App();
+
+foreach ( glob( APP_DIR . "/config/*.php" ) as $file ) {
+	require $file;
+}
