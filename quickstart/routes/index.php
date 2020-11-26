102a55a789eadc81962e0dc795d5a931ccb8d188
Joseph Scott <joseph@josephscott.org>
2020-11-25 17:07:53 -0700

    Format home page

--- quickstart/routes/index.php
+++ quickstart/routes/index.php
@@ -1,6 +1,16 @@
 <?php
 declare( strict_types = 1 );
 
+$out =<<<HTML
+
+<div class="row">
+	<div class="col-md">
+		Hello World!
+	</div>
+</div>
+
+HTML;
+
 echo $app->use( 'twig' )->render( 'header.html' );
-echo "Hello World!";
+echo $out;
 echo $app->use( 'twig' )->render( 'footer.html' );
